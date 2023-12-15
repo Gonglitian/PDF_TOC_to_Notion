4e7a1428a2007182f2f2a65ea420c2c11806fd21
GongLiTian <gongc9@qq.com>
2023-12-15 15:14:48 +0800

    Add .env file to gitignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import fitz  # PyMuPDF
 from notion_client import Client
-notion = Client(auth="secret_uOyI4EWqT2v3Hi2r0LA6N8s7oBArgAaIZbBt9y3Rdzc")
+from dotenv import load_dotenv
+import os
 
 
 def get_children_blocks(pdf_path):
@@ -61,28 +62,29 @@ def add_papers_to_notion_database(database_id, paper_note_template):
                     },
                 ],
             },
+            "Relation": {
+                "relation": [
+                    {
+                        "id": os.environ.get("RELATION_ID"),  # 关联数据库ID
+                    },
+                ],
+            },
             # 如果数据库有其他属性，可以在这里添加
         },
         children=paper_note_template["children_blocks"],
     )
     print(f"Page for '{paper_note_template['title']}' added to the database.")
 
-# def save_markdown(markdown_content, output_path):
-#     if markdown_content is None:
-#         print("没有提取到任何标题，Markdown文件未创建。")
-#         return
-
-#     with open(output_path, 'w', encoding='utf-8') as md_file:
-#         md_file.write('\n'.join(markdown_content))
-#     print(f"Markdown文件已保存到：{output_path}")
-
 
 # 使用示例
-pdf_path = 'src_file\doc1.pdf'  # PDF文件路径
-# markdown_output_path = 'thesis_headings.md'  # Markdown输出文件路径
-# headings = extract_headings(pdf_path)
-# save_markdown(headings, markdown_output_path)
+pdf_path = 'src_file/doc1.pdf'  # PDF文件路径
 # 调用函数
-database_id = "52ab0b36254d45aabf999e8bc8b02366"  # 替换为你的数据库ID
+# READ .ENV FILE
+load_dotenv(override=True)
+
+notion = Client(auth=os.environ.get("NOTION_TOKEN"))
+
 paper_note_template = get_children_blocks(pdf_path)
-add_papers_to_notion_database(database_id, paper_note_template)
+
+add_papers_to_notion_database(os.environ.get(
+    "DATABASE_ID"), paper_note_template)
--- utils.py
+++ utils.py
@@ -1,59 +1,8 @@
-from notion_client import Client
+# def save_markdown(markdown_content, output_path):
+#     if markdown_content is None:
+#         print("没有提取到任何标题，Markdown文件未创建。")
+#         return
 
-
-papers = [
-    {
-        "title": "Paper 1 Title",
-        "toc": ["Introduction", "Method", "Results", "Discussion", "Conclusion"]
-    },
-    {
-        "title": "Paper 2 Title",
-        "toc": ["Background", "The Study", "Analysis", "Findings", "Summary"]
-    },
-    # ...更多论文数据
-]
-
-# 初始化Notion客户端
-notion = Client(auth="secret_uOyI4EWqT2v3Hi2r0LA6N8s7oBArgAaIZbBt9y3Rdzc")
-
-
-def add_papers_to_notion_database(database_id, papers):
-    for paper in papers:
-        # 创建Notion数据库页面
-        new_page = notion.pages.create(
-            parent={"database_id": database_id},
-            properties={
-                "Name": {
-                    "title": [
-                        {
-                            "text": {
-                                "content": paper["title"],  # 论文标题
-                            },
-                        },
-                    ],
-                },
-                # 如果数据库有其他属性，可以在这里添加
-            },
-            children=[
-                {
-                    "object": "block",
-                    "type": "bulleted_list_item",
-                    "bulleted_list_item": {
-                        "rich_text": [
-                            {
-                                "type": "text",
-                                "text": {
-                                    "content": item,  # 目录项
-                                },
-                            },
-                        ],
-                    },
-                } for item in paper["toc"]  # 为目录中的每一项创建一个列表项
-            ],
-        )
-        print(f"Page for '{paper['title']}' added to the database.")
-
-
-# 调用函数
-database_id = "52ab0b36254d45aabf999e8bc8b02366"  # 替换为你的数据库ID
-add_papers_to_notion_database(database_id, papers)
+#     with open(output_path, 'w', encoding='utf-8') as md_file:
+#         md_file.write('\n'.join(markdown_content))
+#     print(f"Markdown文件已保存到：{output_path}")
